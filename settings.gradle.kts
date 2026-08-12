d2c3085cba6cbe5db75d60e2f913fb4ca1a40fa0
hUh63 <3133565923@qq.com>
2026-08-13 02:06:17 +0800

    fix: maven 仓库顺序调整（官方源优先，避免阿里云镜像 502 导致 CI 失败）

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,24 @@
 pluginManagement {
     repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/gradle-plugin")
         maven("https://maven.aliyun.com/repository/central")
-        google()
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        google()
+        mavenCentral()
         maven("https://maven.aliyun.com/repository/google")
         maven("https://maven.aliyun.com/repository/public")
         maven("https://maven.aliyun.com/repository/central")
         maven("https://jitpack.io")
-        google()
-        mavenCentral()
     }
 }
 
